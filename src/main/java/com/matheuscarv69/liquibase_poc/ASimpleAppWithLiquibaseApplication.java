534b550d60b18303906a4103cab08c1762672c10
matheuscarv69 <matheus9126@gmail.com>
2025-04-10 20:25:22 -0400

    :tada: initial commit

---
+++ src/main/java/com/matheuscarv69/liquibase_poc/ASimpleAppWithLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscarv69.liquibase_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ASimpleAppWithLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ASimpleAppWithLiquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuscarv69/liquibase_poc/ASimpleAppWithLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscarv69.liquibase_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ASimpleAppWithLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
